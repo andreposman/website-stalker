717d3310102ff4146c35baa6934ad808416c4ab4
Andre Posman <andre.posman@gmail.com>
2019-09-24 23:03:09 -0300

    refactoring to functions

--- main.go
+++ main.go
@@ -9,25 +9,28 @@ import (
 func main() {
 
 	showIntroduction()
-	showMenu()
 
-	userInput := readUserInput()
+	for {
+		showMenu()
+		userInput := readUserInput()
 
-	switch userInput {
-	case 1:
-		startMonitoring()
-	case 2:
-		fmt.Println("Logs")
+		switch userInput {
+		case 1:
+			startMonitoring()
+		case 2:
+			fmt.Println("Logs")
 
-	case 0:
-		fmt.Println("Saindo do Programa")
-		os.Exit(0) //boa pratica sair com 0
+		case 0:
+			fmt.Println("Saindo do Programa")
+			os.Exit(0) //boa pratica sair com 0
 
-	default:
-		fmt.Println("Error")
-		os.Exit(-1) // erro com o status -1
+		default:
+			fmt.Println("Error")
+			os.Exit(-1) // erro com o status -1
 
+		}
 	}
+
 }
 
 func showIntroduction() {
@@ -36,10 +39,12 @@ func showIntroduction() {
 }
 
 func showMenu() {
-	fmt.Print("-----------------------------------\n\n")
+	fmt.Println("-----------------------------------")
 	fmt.Println("1- Iniciar o Monitoramento")
 	fmt.Println("2- Exibir Logs")
 	fmt.Println("0- Sair do Programa")
+	fmt.Println("-----------------------------------")
+	fmt.Println("")
 }
 
 func readUserInput() byte {
@@ -50,15 +55,24 @@ func readUserInput() byte {
 }
 
 func startMonitoring() {
-	fmt.Println("Start watching...")
-	url := "https://www.google.com"
+	fmt.Println("\nStart watching...")
 
-	response, err := http.Get(url)
+	websites := []string{"http://random-status-code.herokuapp.com/", "https://www.google.com", "http://andreposman.surge.sh/", "https://github.com"}
 
-	fmt.Println(response.Status)
-
-	if err != nil {
-		fmt.Println(err)
+	// o range dos meus sites, podem me retornar duas coisas, o indice e o valor daquela determinada posição
+	for i, url := range websites {
+		fmt.Sprintf("Testing position %v of %v.\n", i, len(websites))
+		fmt.Sprintf("Current website beeing tested is %v\n", url)
+		websiteTesting(url)
 	}
 
+}
+
+func websiteTesting(url string) {
+	response, _ := http.Get(url)
+	if response.StatusCode == 200 {
+		fmt.Println("\n>> ", url, "was successfully loaded.")
+	} else {
+		fmt.Println("Status Code", response.StatusCode, "\nIt seems like the website", url, "is experiencing some problems.")
+	}
 }
